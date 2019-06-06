458d832d56ce65ce8ba9b77bb5daeedf8f6effcc
Administrator <Administrator@DESKTOP-86R829B>
2019-06-06 15:46:40 +0800

    grade模块

---
+++ src/cn/kgc/tangcco/dao/impl/GradeDaoImpl.java
@@ -0,0 +1,12 @@
+package cn.kgc.tangcco.dao.impl;
+
+import cn.kgc.tangcco.dao.GradeDao;
+
+public class GradeDaoImpl implements GradeDao {
+
+	@Override
+	public void sayHello() {
+		System.out.println("gradedao say hello");
+	}
+
+}
